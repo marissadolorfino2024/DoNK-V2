06d19b36aa086ef18db283238a6fb15e138d9de4
Marissa Dolorfino <marissadolorfino2024@gmail.com>
2026-05-06 14:45:00 -0400

    clustering 2

--- src/cluster_pdb-bind.sh
+++ src/cluster_pdb-bind.sh
@@ -9,12 +9,12 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=150GB
 #SBATCH --time=24:00:00
-#SBATCH --error=intermediate/cluster_pdb-bind.err
-#SBATCH --out=intermediate/cluster_pdb-bind.out
+#SBATCH --error=intermediate/cluster_pdb-bind_0.8sim.err
+#SBATCH --out=intermediate/cluster_pdb-bind_0.8sim.out
 
-echo "clustering with sequence identity 0.5 threshold"
+echo "clustering with sequence identity 0.8 threshold"
 
-time foldseek easy-cluster data/pdb-bind-receptor_pdbs/ prod/pdb-bind_clustering intermediate/cluster_tmp --min-seq-id 0.5
+time foldseek easy-cluster data/pdb-bind-receptor_pdbs/ prod/clustering_results_0.8sim/pdb-bind_clustering intermediate/cluster_tmp --min-seq-id 0.8
 
 echo "clustering complete!"
 
